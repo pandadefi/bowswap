4559213e10e1352d6978ae0de4ff6fba14b47cec
panda <87183122+pandadefi@users.noreply.github.com>
2021-08-10 11:43:37 +0200

    speed up tests

--- tests/swap_test.py
+++ tests/swap_test.py
@@ -6,24 +6,16 @@ def transfer(token, amount, whale, to):
     token.transfer(to, amount, {"from": whale})
 
 
-def test_view(vault_swapper, vault_from, vault_to, amount):
-    print("Estimate: " + vault_from.name() + " to " + vault_to.name())
-    estimate = vault_swapper.estimate_out(vault_from, vault_to, amount)
-    assert estimate > 0
-
-
 def test_swap(user, vault_from, vault_to, whale, vault_swapper, amount):
-    print("Testing transfer: ")
     transfer(vault_from, amount, whale, user)
     vault_from.approve(vault_swapper, amount, {"from": user})
+    # gets a estimate of the amount out
     estimate = vault_swapper.estimate_out(vault_from, vault_to, amount)
+    # Makes sure it revert if amout out is too small
+    with brownie.reverts():
+        vault_swapper.swap(vault_from, vault_to, amount, amount * 1.1, {"from": user})
+
+    # Do the swap
     vault_swapper.swap(vault_from, vault_to, amount, estimate * 0.999, {"from": user})
     assert vault_to.balanceOf(user) > estimate * 0.999
     print(vault_to.balanceOf(user))
-
-
-def test_swap_revert(user, vault_from, vault_to, whale, vault_swapper, amount):
-    transfer(vault_from, amount, whale, user)
-    vault_from.approve(vault_swapper, amount, {"from": user})
-    with brownie.reverts():
-        vault_swapper.swap(vault_from, vault_to, amount, amount * 1.1, {"from": user})
