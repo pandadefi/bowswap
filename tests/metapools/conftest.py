022e9df8c085761d538e01a59618275ebc69a7f1
panda <87183122+pandadefi@users.noreply.github.com>
2021-09-03 16:26:48 +0200

    test EUR swaps

--- tests/metapools/conftest.py
+++ tests/metapools/conftest.py
@@ -8,8 +8,8 @@ CRV_META_3USD_VAULT = [
     "0xC4dAf3b5e2A9e93861c3FBDd25f1e943B8D87417",
     "0x8cc94ccd0f3841a468184aCA3Cc478D2148E1757",
     "0x5fA5B62c8AF877CB37031e0a3B2f34A78e3C56A6",
-    "0x30FCf7c6cDfC46eC237783D94Fc78553E79d4E9C",
-    # "0xf8768814b88281DE4F532a3beEfA5b85B69b9324", # removed for speed~ `QMMMMMMMM`
+    # "0x30FCf7c6cDfC46eC237783D94Fc78553E79d4E9C", # removed for speed
+    # "0xf8768814b88281DE4F532a3beEfA5b85B69b9324", 
     # "0x054AF22E1519b020516D72D749221c24756385C9",
     # "0x3B96d491f067912D18563d56858Ba7d6EC67a6fa",
     # "0x6Ede7F19df5df6EF23bD5B9CeDb651580Bdf56Ca",
@@ -21,13 +21,17 @@ CRV_META_BTC_VAULT = [
     "0x8fA3A9ecd9EFb07A8CE90A6eb014CF3c0E3B32Ef",
     "0xe9Dc63083c464d6EDcCFf23444fF3CFc6886f6FB",
     "0x3c5DF3077BcF800640B5DAE8c91106575a4826E6",
-    "0x23D3D0f1c697247d5e0a9efB37d8b0ED0C464f7f",
+    # "0x23D3D0f1c697247d5e0a9efB37d8b0ED0C464f7f", # removed for speed
+]
+
+EUR_POOLS = [ # Not real metapool but coin 1 = sEUR so that can used same code
+    "0x25212Df29073FfFA7A67399AcEfC2dd75a831A1A",
+    "0x0d4EA8536F9A13e4FBa16042a46c30f092b06aA5"
 ]
 
 ALL_PAIRS = list(itertools.combinations(CRV_META_BTC_VAULT, 2)) + list(
     itertools.combinations(CRV_META_3USD_VAULT, 2)
-)
-
+) + itertools.combinations(EUR_POOLS, 2)
 
 @pytest.fixture
 def gov(accounts):
@@ -67,10 +71,10 @@ def whale(vault_from):
 
 @pytest.fixture
 def amount(vault_from):
-    if vault_from.address in CRV_META_3USD_VAULT:
-        yield 1000 * 10 ** vault_from.decimals()  # 1000 USD
-    else:
+    if vault_from.address in CRV_META_BTC_VAULT:
         yield 0.1 * 10 ** vault_from.decimals()  # 0.1 BTC
+    else:
+        yield 1000 * 10 ** vault_from.decimals()  # 1000 USD/EUR
 
 
 @pytest.fixture
