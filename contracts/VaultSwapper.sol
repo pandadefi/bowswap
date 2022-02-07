207b56b711633de1447e2fbba80d98cd39f49144
Major-Eth <major-tom.eth@pm.me>
2022-02-07 15:50:44 +0100

    🚨 Underlying instead of target

--- contracts/VaultSwapper.sol
+++ contracts/VaultSwapper.sol
@@ -223,7 +223,7 @@ contract VaultSwapper is Initializable {
             amount,
             address(this)
         );
-        remove_liquidity_one_coin(target, underlying_pool, underlying_amount, 1, 1);
+        remove_liquidity_one_coin(underlying, underlying_pool, underlying_amount, 1, 1);
 
 
         IERC20 underlying_coin = IERC20(_get_coin(underlying_pool, 1));
