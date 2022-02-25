152b356eb9e5fbf21215f3d6bf918d04da7616e9
Major <90963895+Major-Eth@users.noreply.github.com>
2022-02-25 10:39:50 +0100

    [Fix] Fixing tests (#10)
    * ✅ Updating tests
    
    * ✅ Updating addr

--- tests/yvempire/aave_swap_test.py
+++ tests/yvempire/aave_swap_test.py
@@ -3,7 +3,7 @@ from brownie import Contract
 
 
 def test_v1_swap(user, ausdt, get_ausdt, yv_empire):
-    amount = 10_000 * 10 ** 6
+    amount = 1_000 * 10 ** 6
     get_ausdt(user, amount)
     ausdt.approve(yv_empire, amount, {"from": user})
     yv_empire.migrate([(1, ausdt)], {"from": user})
@@ -12,9 +12,9 @@ def test_v1_swap(user, ausdt, get_ausdt, yv_empire):
 
 
 def test_v2_swap(user, ausdcv2, get_ausdcv2, yv_empire):
-    amount = 10_000 * 10 ** 6
+    amount = 1_000 * 10 ** 6
     get_ausdcv2(user, amount)
     ausdcv2.approve(yv_empire, amount, {"from": user})
     yv_empire.migrate([(2, ausdcv2)], {"from": user})
-    vault = Contract("0x5f18C75AbDAe578b483E5F43f12a39cF75b973a9")
+    vault = Contract("0xa354F35829Ae975e850e23e9615b11Da1B3dC4DE")
     assert vault.balanceOf(user) != 0
