161d05983b9721fec4325d5d09bd69a817b5f0a4
panda <87183122+pandadefi@users.noreply.github.com>
2022-12-20 15:22:45 +0100

    feat: add views

--- tests/yvempire/aave_swap_test.py
+++ tests/yvempire/aave_swap_test.py
@@ -6,33 +6,41 @@ def test_v1_swap(user, registry, usdt, ausdt, get_ausdt, yv_empire):
     amount = 1_000 * 10**6
     get_ausdt(user, amount)
     ausdt.approve(yv_empire, amount, {"from": user})
-    yv_empire.migrate["tuple"]((1, ausdt), {"from": user})
+    estimate = yv_empire.estimate["tuple"]((1, ausdt, amount))
+    yv_empire.migrate["tuple"]((1, ausdt, amount), {"from": user})
     vault = Contract(registry.latestVault(usdt))
     assert vault.balanceOf(user) != 0
+    assert pytest.approx(vault.balanceOf(user)) == estimate
 
 
 def test_v1_swap_many(user, registry, usdt, ausdt, get_ausdt, yv_empire):
     amount = 1_000 * 10**6
     get_ausdt(user, amount)
     ausdt.approve(yv_empire, amount, {"from": user})
-    yv_empire.migrate["tuple[]"]([(1, ausdt)], {"from": user})
+    [estimate] = yv_empire.estimate["tuple[]"]([(1, ausdt, amount)])
+    yv_empire.migrate["tuple[]"]([(1, ausdt, amount)], {"from": user})
     vault = Contract(registry.latestVault(usdt))
     assert vault.balanceOf(user) != 0
+    assert pytest.approx(vault.balanceOf(user)) == estimate
 
 
 def test_v2_swap(user, registry, usdc, ausdcv2, get_ausdcv2, yv_empire):
     amount = 1_000 * 10**6
     get_ausdcv2(user, amount)
     ausdcv2.approve(yv_empire, amount, {"from": user})
-    yv_empire.migrate["tuple"]((2, ausdcv2), {"from": user})
+    estimate = yv_empire.estimate["tuple"]((2, ausdcv2, amount))
+    yv_empire.migrate["tuple"]((2, ausdcv2, amount), {"from": user})
     vault = Contract(registry.latestVault(usdc))
     assert vault.balanceOf(user) != 0
+    assert pytest.approx(vault.balanceOf(user)) == estimate
 
 
 def test_v2_swap_many(user, registry, usdc, ausdcv2, get_ausdcv2, yv_empire):
     amount = 1_000 * 10**6
     get_ausdcv2(user, amount)
     ausdcv2.approve(yv_empire, amount, {"from": user})
-    yv_empire.migrate["tuple[]"]([(2, ausdcv2)], {"from": user})
+    [estimate] = yv_empire.estimate["tuple[]"]([(2, ausdcv2, amount)])
+    yv_empire.migrate["tuple[]"]([(2, ausdcv2, amount)], {"from": user})
     vault = Contract(registry.latestVault(usdc))
     assert vault.balanceOf(user) != 0
+    assert pytest.approx(vault.balanceOf(user)) == estimate
--- tests/yvempire/compound_swap_test.py
+++ tests/yvempire/compound_swap_test.py
@@ -7,6 +7,8 @@ def test_swap(user, cusdc, get_cusdc, yv_empire):
     amount = 1_000 * 10**8
     get_cusdc(user, amount)
     cusdc.approve(yv_empire, amount, {"from": user})
-    yv_empire.migrate["tuple[]"]([(0, cusdc)], {"from": user})
+    [estimate] = yv_empire.estimate["tuple[]"]([(0, cusdc, amount)])
+    yv_empire.migrate["tuple[]"]([(0, cusdc, amount)], {"from": user})
     vault = Contract("0xa354F35829Ae975e850e23e9615b11Da1B3dC4DE")
     assert vault.balanceOf(user) != 0
+    assert pytest.approx(vault.balanceOf(user)) == estimate
